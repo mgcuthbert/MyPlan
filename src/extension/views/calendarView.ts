8eb1d32746f91935fdff6f77354e668b40616e22
Michael Cuthbert <mcuthbert@netflix.com>
2023-05-27 13:52:12 -0700

    Some more updates to sort out the Calendar view

--- src/extension/views/calendarView.ts
+++ src/extension/views/calendarView.ts
@@ -1,8 +1,9 @@
 import { months } from "../utils";
 
 export function buildCalendarView(planName:string, planData:any, useMi:boolean, planGoal:string) {
-    let header = document.getElementById("calendar-header");
+    let header = document.getElementById("calendar-header") as HTMLElement;
     if (header) {
+        addNextPreviousEventListener(header, planName, planData, useMi, planGoal);
         let year = Number((header.querySelector("h2.heading-nav span.nav-group a") as HTMLElement).innerText);
         let monthElement = header.querySelector("h2.heading-nav span.active span.month-display") as HTMLElement;
         if (monthElement) {
@@ -10,7 +11,7 @@ export function buildCalendarView(planName:string, planData:any, useMi:boolean, 
             let monthNum = -1;
             for (let m of months) {
                 monthNum += 1;
-                if (m === month) {
+                if (m === month || m.substring(0, 3) === month) {
                     break; 
                 }
             }
@@ -20,16 +21,15 @@ export function buildCalendarView(planName:string, planData:any, useMi:boolean, 
                 return isDateInYearAndMonth(new Date(Number(millis)), year, monthNum);
             }).sort();
             addGoal(planGoal);
-
+            
             // now that I have the filtered data, I need to find where to put each piece of data.
             let calendarTBody = document.querySelector("div.view div.page div.content table.month-calendar tbody") as HTMLElement;
+            const firstDayOfMonth = new Date(year, monthNum, 1).getUTCDay();
             for (let key of filteredKeys) {
                 const currentData = planData.data[key];
-                console.log(currentData);
                 const newElement = buildNewTrainingElement(currentData, useMi);
-                const currentDayOfMonth = new Date(currentData.date).getUTCDate();
-                console.log(currentDayOfMonth);
-                const {row, column} = getRowAndColumn(currentDayOfMonth);
+                const currentDay = new Date(currentData.date).getUTCDate();
+                const {row, column} = getRowAndColumn(currentDay, firstDayOfMonth);
                 const insertPoint = calendarTBody.querySelector(`tr:nth-child(${row}) td:nth-child(${column}) ul.activities`) as HTMLElement;
                 insertPoint.appendChild(newElement);
             }
@@ -37,11 +37,19 @@ export function buildCalendarView(planName:string, planData:any, useMi:boolean, 
     }
 };
 
+function addNextPreviousEventListener(header:HTMLElement, planName:string, planData:any, useMi:boolean, planGoal:string) {
+    const nextButton = header.querySelector("h2.heading-nav span.active a.nav-arrow-icon.next") as HTMLElement;
+    nextButton.addEventListener('click', () => buildCalendarView(planName, planData, useMi, planGoal));
+
+    const prevButton = header.querySelector("h2.heading-nav span.active a.nav-arrow-icon.prev") as HTMLElement;
+    prevButton.addEventListener('click', () => buildCalendarView(planName, planData, useMi, planGoal));
+}
+
 function addGoal(planGoal:string) {
     const insertPoint = document.querySelector("body div.view div.page");
     let goalElement = document.createElement("div");
     goalElement.className = "mb-sm";
-    goalElement.innerHTML = `<b>Goal:</b>${planGoal}`;
+    goalElement.innerHTML = `<b>Goal: </b>${planGoal}`;
     insertPoint?.firstElementChild?.insertAdjacentElement("afterend", goalElement);
 }
 
@@ -59,16 +67,20 @@ function buildNewTrainingElement(data:any, useMi:boolean): HTMLElement {
 }
 
 function isDateInYearAndMonth(date:Date, year:number, month:number) {
-    const dateYear = date.getFullYear();
-    const dateMonth = date.getMonth();
+    const dateYear = date.getUTCFullYear();
+    const dateMonth = date.getUTCMonth();
     return dateYear === year && dateMonth === month;
 }
 
-function getRowAndColumn(dayOfMonth:number) {
+function getRowAndColumn(dayOfMonth:number, startDayOfWeek:number) {
     const numCols = 7; // Number of columns in the calendar grid
-  
     // Calculate the row and column based on the day of the month
-    const row = Math.ceil(dayOfMonth / numCols);
-    const column = dayOfMonth - (numCols * (row-1));
+    let updatedDay = startDayOfWeek;
+    if (startDayOfWeek === 0) {
+        updatedDay = 7;
+    }
+    let updateDayOfMonth = dayOfMonth + updatedDay - 1;
+    const row = Math.ceil(updateDayOfMonth / numCols);
+    const column = updateDayOfMonth - (numCols * (row-1));
     return { row, column };
 }
